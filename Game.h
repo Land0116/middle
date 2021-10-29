63dddc23836cdaa2070b2a42b495cf4890d3705e
20201287강민혁 <83389456+Land0116@users.noreply.github.com>
2021-10-29 02:29:35 +0000

    Game클래스

---
+++ Game.h
@@ -0,0 +1,29 @@
+#ifndef Game_H
+#define Game_H
+
+#include <iostream>
+#include <SDL.h>
+
+class Game
+{
+  public : 
+    Game() {}
+    ~Game() {}
+
+    bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+    void render();
+    void update();
+    bool running();
+    void handleEvents();
+    void clean();
+
+  private :
+    SDL_Window* m_pWindow = 0;
+    SDL_Renderer* m_pRenderer = 0;
+
+    bool m_bRunning = false;
+    
+    int countColor = 1;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,74 +1,19 @@
-//[Hello SDL분리하기]
-#include <SDL.h>
+//[Game클래스]
+#include "Game.h"
 
-int main(int argc, char* args[]);
-bool init(const char* title, int xpos, int ypos, int width, int hegiht, int flags);
-void update();
-void render();
-
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false; //게임 루프를 실행 시킬 수 있게 한다.
-int countColor = 1;
+Game* g_game = 0;
 
 int main(int argc, char* args[])
 {
- 
-   if (init("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 512, 512, SDL_WINDOW_SHOWN))
-   {
-     g_bRunning = true;
-
-   }
-    else {
-        return 1;
-    }
-
-  while(g_bRunning)
+  g_game = new Game();
+  g_game->init("Game Class", 100, 100, 640, 480, 0);
+  while(g_game->running())
   {
-    update();
-    if(countColor == 10)
-    {
-      g_bRunning = false;
-    }
-    render();
+    g_game->handleEvents();
+    g_game->update();
+    g_game->render();
   }
-  SDL_Delay(5000);
-  SDL_Quit();
+  g_game->clean();
 
     return 0;
-}
-
-bool init(const char* title, int xpos, int ypos, int width, int hegiht, int flags)
-{
-  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-  {
-    g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, hegiht, flags);
-
-    if(g_pWindow != 0)
-    {
-      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-    }
-  }
-  else { return false; }
-
-  //SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
-
-  return true;
-}
-
-void update()
-{
-  if(countColor <= 10)
-  {
-    SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
-    SDL_Delay(1000);
-  }
-  countColor++;
-}
-
-void render()
-{
-  SDL_RenderClear(g_pRenderer);
-  SDL_RenderPresent(g_pRenderer);
 }
